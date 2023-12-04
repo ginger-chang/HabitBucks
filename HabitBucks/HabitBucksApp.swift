a86fdfd558328a77f49716c39a9e8820824488af
Ginger Chang <ginger_chang@berkeley.edu>
2023-12-03 20:40:21 -0800

    Initial Commit

---
+++ HabitBucks/HabitBucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitBucksApp.swift
+//  HabitBucks
+//
+//  Created by Ginger Chang on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitBucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
